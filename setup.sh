79be36acba3c84eef500acde8d2ae7e5c97d8cec
Rick Burgess <rick@dynamic.do>
2016-08-31 12:33:52 +0100

    added set-up script (do not run on creative-bolierplate repo)

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+NamePlaceholder="creative-boilerplate"
+echo -n "Enter project name and press [ENTER]:"
+read project
+sed "s/$NamePlaceholder/$project/g" "./package.json" > "./package.json.tmp"
+mv "./package.json.tmp" "package.json"
+
+DescPlaceholder="SmartContent Creative Boilerplate"
+echo -n "Enter project description and press [ENTER]:"
+read desc
+sed "s/$DescPlaceholder/$desc/g" "./package.json" > "./package.json.tmp"
+mv "./package.json.tmp" "package.json"
+
+echo -n "Enter git repo target and press [ENTER]:"
+read origin
+
+rm -rf ".git"
+git init
+git add .
+git commit -am "Initial commit by setup.sh"
+git remote add origin "$origin"
+git push -u origin master
+
+npm install
