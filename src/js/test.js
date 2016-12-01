7667225cbb3b3e0302521f43d58731681ccf4caf
Tom Castleman <tom@b3it.co>
2016-12-01 16:53:55 +0000

    Update for CDK

--- src/js/test.js
+++ src/js/test.js
@@ -2,7 +2,8 @@ import events from 'smartcontent-cdk/lib/events';
 import meta from '../../test/data/meta';
 import campaign from '../../test/data/campaign';
 import dataItems from '../../test/data/data';
+import rulesets from '../../test/data/rulesets';
 
 document.addEventListener('DOMContentLoaded', () => {
-  events(meta, campaign, dataItems);
+  events(meta, campaign, dataItems, rulesets);
 });
