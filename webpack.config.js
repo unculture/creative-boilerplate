6ae3ed0850477287db8268d4b32b72fda2616a7c
Susan Sun <susansun@Susans-MacBook-Pro-2.local>
2017-11-24 12:23:15 +0000

    add events plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 const merge = require('webpack-merge');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 let baseConfig
+const eventsConfig = require('smartcontent-cdk/webpack/webpack.config.events')
+let webpackConfig
 const path = require("path")
 
 if (process.env.NODE_ENV !== 'production') {
  	baseConfig = require('smartcontent-cdk/webpack/webpack.config.dev');
+	webpackConfig = merge(baseConfig, {});
 }
 else {
 	baseConfig = require('smartcontent-cdk/webpack/webpack.config.prod');
+	webpackConfig = [
+		merge(baseConfig, {}), 
+		eventsConfig
+	]
 }
 
-module.exports = merge(baseConfig, {});
+module.exports = webpackConfig
 
