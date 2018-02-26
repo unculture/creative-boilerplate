d4bf91000ccb69f400c1805ca5230163d8fbc01d
Gon Fernandez <gon.fernandez@gmail.com>
2018-02-26 17:11:27 +0000

    Valid initial test data - to be used or to take as reference, it is commented out to begin with

--- test/data/campaign.js
+++ test/data/campaign.js
@@ -1,1 +1,493 @@
-export default {};
+export default {
+    // data: {
+    //   "nationalRailData": {
+    //     "euston-departures": {
+    //       "generatedAt": "2017-12-05T16:39:04.2940016+00:00",
+    //       "station": "EUS",
+    //       "arrivals": [],
+    //       "departures": [
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Watford Junction",
+    //             "code": "WFJ"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "16:21",
+    //           "etd": "On time",
+    //           "operator": "London Overground",
+    //           "operatorCode": "LO"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Manchester Piccadilly",
+    //             "code": "MAN"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "12:40",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Birmingham New Street",
+    //             "code": "BHM"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "12:02",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Edinburgh",
+    //             "code": "EDB"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "00:43",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Crewe",
+    //             "code": "CRE"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "16:46",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Birmingham New Street",
+    //             "code": "BHM"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "16:50",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Tring",
+    //             "code": "TRI"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "16:54",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Glasgow Central",
+    //             "code": "GLC"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "16:57",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Watford Junction",
+    //             "code": "WFJ"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "16:57",
+    //           "etd": "On time",
+    //           "operator": "London Overground",
+    //           "operatorCode": "LO"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Manchester Piccadilly",
+    //             "code": "MAN"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:00",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Birmingham New Street",
+    //             "code": "BHM"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:03",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Milton Keynes Central",
+    //             "code": "MKC"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:05",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Liverpool Lime Street",
+    //             "code": "LIV"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:07",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Holyhead",
+    //             "code": "HHD"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:10",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Tring",
+    //             "code": "TRI"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:12",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Birmingham New Street",
+    //             "code": "BHM"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:13",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Northampton",
+    //             "code": "NMP"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:16",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Watford Junction",
+    //             "code": "WFJ"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:17",
+    //           "etd": "On time",
+    //           "operator": "London Overground",
+    //           "operatorCode": "LO"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Manchester Piccadilly",
+    //             "code": "MAN"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:20",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Birmingham New Street",
+    //             "code": "BHM"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:23",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Birmingham New Street",
+    //             "code": "BHM"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:24",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Glasgow Central",
+    //             "code": "GLC"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:30",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Milton Keynes Central",
+    //             "code": "MKC"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:30",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Liverpool Lime Street",
+    //             "code": "LIV"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:33",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Watford Junction",
+    //             "code": "WFJ"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:34",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Watford Junction",
+    //             "code": "WFJ"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:37",
+    //           "etd": "On time",
+    //           "operator": "London Overground",
+    //           "operatorCode": "LO"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Manchester Piccadilly",
+    //             "code": "MAN"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:40",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Tring",
+    //             "code": "TRI"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:41",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Glasgow Central",
+    //             "code": "GLC"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:43",
+    //           "etd": "On time",
+    //           "operator": "Virgin Trains",
+    //           "operatorCode": "VT"
+    //         },
+    //         {
+    //           "origin": {
+    //             "name": "London Euston",
+    //             "code": "EUS"
+    //           },
+    //           "destination": {
+    //             "name": "Crewe",
+    //             "code": "CRE"
+    //           },
+    //           "sta": "",
+    //           "eta": "",
+    //           "std": "17:46",
+    //           "etd": "On time",
+    //           "operator": "London Midland",
+    //           "operatorCode": "LM"
+    //         }
+    //       ]
+    //     }
+    //   }
+    // }
+};
--- test/data/data.js
+++ test/data/data.js
@@ -1,1 +1,80 @@
-export default [];
+export default {
+	// "data": [
+  //   {
+  // 	    "id": "14cf0b3e-5611-49c4-a65f-9bb19967524a7891",
+  // 	    "type": "hash_tagd",
+  // 	    "name": "Hastagd Submission: 990678429864849887_1821288867",
+  // 	    "fields": [
+  // 	      {
+  // 	        "id": "990678429864849887_18212888677891"
+  // 	      },
+  // 	      {
+  // 	        "screen_fullname": "\u2728Goku The Shiba Inu \u2728"
+  // 	      },
+  // 	      {
+  // 	        "screen_name": "shibainu.goku"
+  // 	      },
+  // 	      {
+  // 	        "cache": "più &ugrave;Yeah keep talking, I'm listening.. #shiba #doglife #animalsofinstagram #dogsofinstagram #shibamania #fox #cutepuppy #puppy #cutepuppies #cuteanimals #cute #shibainu #shibainusofinstagram #instapup #instapuppy #webstagram"
+  // 	      },
+  // 	      {
+  // 	        "screen_pic": ""
+  // 	      },
+  // 	      {
+  // 	        "conduit": "Instagram"
+  // 	      },
+  // 	      {
+  // 	        "type": "Instagram photo"
+  // 	      },
+  // 	      {
+  // 	        "media_image": "..\/test\/assets\/test1.jpeg"
+  // 	      },
+  // 	      {
+  // 	        "media_video": ""
+  // 	      },
+  // 	      {
+  // 	        "created_at": "2015-05-20 18:08:13"
+  // 	      }
+  // 	    ],
+  // 	    "targeting": []
+  // 	  },
+  // 		{
+  // 	    "id": "14cf0b3e-5611-49c4-a65f-9bb19967524a7892",
+  // 	    "type": "hash_tagd",
+  // 	    "name": "Hastagd Submission: 990678429864849887_18212888672",
+  // 	    "fields": [
+  // 	      {
+  // 	        "id": "990678429864849887_18212888677892"
+  // 	      },
+  // 	      {
+  // 	        "screen_fullname": "\u2728Goku The Shiba Inu \u2728"
+  // 	      },
+  // 	      {
+  // 	        "screen_name": "shibainu.goku"
+  // 	      },
+  // 	      {
+  // 	        "cache": "più &ugrave;Yeah keep talking, I'm listening.. #shiba #doglife #animalsofinstagram #dogsofinstagram #shibamania #fox #cutepuppy #puppy #cutepuppies #cuteanimals #cute #shibainu #shibainusofinstagram #instapup #instapuppy #webstagram"
+  // 	      },
+  // 	      {
+  // 	        "screen_pic": ""
+  // 	      },
+  // 	      {
+  // 	        "conduit": "Instagram"
+  // 	      },
+  // 	      {
+  // 	        "type": "Instagram photo"
+  // 	      },
+  // 	      {
+  // 	        "media_image": "..\/test\/assets\/test2.jpeg"
+  // 	      },
+  // 	      {
+  // 	        "media_video": ""
+  // 	      },
+  // 	      {
+  // 	        "created_at": "2015-05-21 18:08:13"
+  // 	      }
+  // 	    ],
+  // 	    "targeting": []
+  // 	  }
+	//   ]
+};
